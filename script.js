59a8ce571ea4236eace08d0069aec45429d3f653
phenomenalgames9-art <phenomenalgames9@gmail.com>
2025-09-24 09:43:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// WordPress Emoji Settings and Detection Script
+window._wpemojiSettings = {
+    "baseUrl": "https:\/\/s.w.org\/images\/core\/emoji\/16.0.1\/72x72\/",
+    "ext": ".png",
+    "svgUrl": "https:\/\/s.w.org\/images\/core\/emoji\/16.0.1\/svg\/",
+    "svgExt": ".svg",
+    "source": {
+        "concatemoji": "https:\/\/msit.edu.in\/wp-includes\/js\/wp-emoji-release.min.js?ver=6.8.2"
+    }
+};
+
+/*! This file is auto-generated */
+! function(s, n) {
+    var o, i, e;
+    function c(e) { /* ... (complex emoji detection logic) ... */ }
+    // ... (the rest of the long, self-executing function) ...
+}((window, document), window._wpemojiSettings);
